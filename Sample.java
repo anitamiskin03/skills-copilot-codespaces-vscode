ce797ec5a85b9092a165755c0dc11e795fc95e3a
anitamiskin03 <124915448+anitamiskin03@users.noreply.github.com>
2025-04-08 12:04:10 +0000

    Copilot First Commit

---
+++ Sample.java
@@ -0,0 +1,10 @@
+public class Sample {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Copilot");
+        Add(5, 10);
+        System.out.println("Sum: " + Add(5, 10));
+    }
+    public static int Add(int a, int b) {
+        return a + b;
+    }
+}
